5903abc457d54b84d5820a5aaaecfa2e678cb7bd
Денис <eltimccc@yandex.ru>
2022-07-06 12:55:45 +0300

    загрузил

---
+++ yatube/posts/urls.py
@@ -0,0 +1,28 @@
+# posts/urls.py
+from django.urls import path
+
+from . import views
+
+
+app_name = 'posts'
+
+urlpatterns = [
+    path('',
+         views.index, name='index'),
+    path('group/<slug>/',
+         views.group_posts, name='group_list'),
+    # Профайл пользователя
+    path('profile/<str:username>/',
+         views.profile, name='profile'),
+    # Просмотр записи
+    path('posts/<int:post_id>/',
+         views.post_detail, name='post_detail'),
+    # Создание поста
+    path('create/',
+         views.post_create, name='post_create'),
+    # Редактирование поста
+    path('posts/<int:post_id>/edit/',
+         views.post_edit, name='update_post'),
+    path('posts/<int:post_id>/comment/',
+         views.add_comment, name='add_comment'),     
+]
---
+++ yatube/yatube/urls.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+from posts import views
+
+handler404 = 'core.views.page_not_found'
+
+urlpatterns = [
+    # импорт правил из приложения posts
+    path('auth/', include('users.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('', include('posts.urls', namespace='posts')),
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('about/', include('about.urls', namespace='about')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
