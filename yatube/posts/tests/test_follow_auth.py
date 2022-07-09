ad4cfbc5769465263ec64bdfbb8fd232869967b1
Денис <eltimccc@yandex.ru>
2022-07-09 11:19:52 +0300

    follow

---
+++ yatube/posts/tests/test_follow_auth.py
@@ -0,0 +1,30 @@
+from django.test import Client, TestCase
+from posts.models import Post, User
+from django.contrib.auth import get_user_model
+from posts.models import Post, User, Follow
+
+
+User = get_user_model()
+
+
+class FollowTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='Ss')
+        cls.post = Post.objects.create(
+            author=cls.user,
+            text='Тестовая пост for follow',
+        )
+        cls.author = cls.user
+
+    def setUp(self):
+        self.user_1 = User.objects.create_user(username='follower')
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.user_1)
+
+    def test_folow(self):
+        followers = Follow.objects.all()
+        follower = Follow.objects.create(
+            user=self.user, author=self.author)
+        self.assertEqual(follower, followers[0])
